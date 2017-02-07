b6082e51fae896d9862a450bd66efb7611e24bfb
Yunzhe <alvence1991@gmail.com>
2017-02-08 10:39:13 +1100

    1

--- CPXC/src/com/yunzhejia/pattern/patternmining/RFPatternMiner.java
+++ CPXC/src/com/yunzhejia/pattern/patternmining/RFPatternMiner.java
@@ -1,10 +1,13 @@
 package com.yunzhejia.pattern.patternmining;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import com.yunzhejia.cpxc.util.DataUtils;
 import com.yunzhejia.pattern.ICondition;
 import com.yunzhejia.pattern.IPattern;
 import com.yunzhejia.pattern.NominalCondition;
@@ -20,12 +23,34 @@ import weka.core.Instances;
 import weka.core.Utils;
 import weka.core.converters.ConverterUtils.DataSource;
 
-public class ApriorPatternMiner implements IPatternMiner {
-
+public class RFPatternMiner implements IPatternMiner {
+	
+	
 	@Override
 	public PatternSet minePattern(Instances data, double minSupp) {
-		// TODO Auto-generated method stub
-		return null;
+		RandomForest rf = new RandomForest();
+		List<IPattern> patternSet = new ArrayList<>();
+		
+		try {
+			rf.buildClassifier(data);
+			Classifier[] trees = rf.getClassifiers();
+			for(Classifier cl:trees){
+				RandomTree tree = (RandomTree)cl;
+				Set<IPattern> patterns = new HashSet<>();
+				generatePatterns(tree.m_Tree, tree.m_Info, patterns,null, minSupp );
+				for (IPattern pattern:patterns){
+					if (!patternSet.contains(pattern)){
+						patternSet.add(pattern);
+					}
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return new PatternSet(patternSet);
 	}
 
 	@Override
@@ -34,76 +59,29 @@ public class ApriorPatternMiner implements IPatternMiner {
 		return null;
 	}
 	
-	public static void main(String[] args){
-		try {
-			DataSource source;
-			Instances data;
-			
-			DataSource sourceTest;
-			Instances dataTest;
-//			String[] files = {"data/synthetic2.arff"};
-			String file="data/synthetic2.arff";
-			String fileTest="data/sick/test.arff";
-//			source = new DataSource("data/synthetic2.arff");
-			source = new DataSource(file);
-			sourceTest = new DataSource(fileTest);
-//			source = new DataSource("data/iris.arff");
-			data = source.getDataSet();
-			dataTest = source.getDataSet();
-			
-			if (data.classIndex() == -1){
-				data.setClassIndex(data.numAttributes() - 1);
-			}
-			if (dataTest.classIndex() == -1){
-				dataTest.setClassIndex(dataTest.numAttributes() - 1);
-			}
-		
-			RandomForest rf = new RandomForest();
-			
-			rf.buildClassifier(data);
-			System.out.println(rf.getClassifiers().length);
-			Classifier[] trees = rf.getClassifiers();
-			RandomTree tree = (RandomTree)trees[0];
-			System.out.println(tree);
-			outputTree(tree.m_Tree);
-			
-			Set<IPattern> patterns = new HashSet<>();
-			generatePatterns(tree.m_Tree, tree.m_Info, patterns,null, data.numAttributes()*0.01);
-			for(IPattern p : patterns){
-				System.out.println(p + "  sup="+ p.support(data));
-			}
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	static int c = 0;
-	public static void outputTree(Tree tree) throws Exception{
-		if (tree.m_Attribute == -1){
-			System.out.println( "classDist: " + Arrays.toString(tree.m_ClassDistribution)+"   count"+c++);
-		}else{
-			System.out.print("("+tree.m_Attribute + "<" + tree.m_SplitPoint+")  AND  ");
-			outputTree(tree.m_Successors[0]);
-			System.out.print("("+tree.m_Attribute + ">=" + tree.m_SplitPoint+")   AND  ");
-			outputTree(tree.m_Successors[1]);
-		}
-	}
-	
+//	public static void outputTree(Tree tree) throws Exception{
+//		if (tree.m_Attribute == -1){
+//			System.out.println( "classDist: " + Arrays.toString(tree.m_ClassDistribution)+"   count"+c++);
+//		}else{
+//			System.out.print("("+tree.m_Attribute + "<" + tree.m_SplitPoint+")  AND  ");
+//			outputTree(tree.m_Successors[0]);
+//			System.out.print("("+tree.m_Attribute + ">=" + tree.m_SplitPoint+")   AND  ");
+//			outputTree(tree.m_Successors[1]);
+//		}
+//	}
+//	
 	
 
-	public static void generatePatterns(Tree tree, Instances m_Info, Set<IPattern> patterns, Set<ICondition> conditions, double minSupport) throws Exception{
-		if (patterns == null){
-			patterns = new HashSet<>();
-		}
+	public void generatePatterns(Tree tree, Instances m_Info, Set<IPattern> patterns, Set<ICondition> conditions, double minSupport) throws Exception{
 		if (conditions == null){
 			conditions = new HashSet<>();
 		}
 		if (tree.m_Attribute == -1){
 			if(conditions != null && !conditions.isEmpty()){
+				System.out.println(tree.m_ClassDistribution);
 				if(tree.m_ClassDistribution[Utils.maxIndex(tree.m_ClassDistribution)] >= minSupport){
 					patterns.add(new Pattern(conditions));
 				}
-				return;
 			}
 		}else if (m_Info.attribute(tree.m_Attribute).isNominal()) {
 
@@ -136,6 +114,19 @@ public class ApriorPatternMiner implements IPatternMiner {
 	        	  newConditions.add(condition);
 	        	  generatePatterns(tree.m_Successors[i],m_Info,patterns,newConditions, minSupport);
 	          }
+		}
+	}
+	public static void main(String[] args){
+		try {
+			Instances data = DataUtils.load("data/sick.arff");
+			PatternSet ps = new RFPatternMiner().minePattern(data, data.numInstances()*0.1);
+			System.out.println(ps.size());
+			for(IPattern p:ps){
+//				System.out.println(pf);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
